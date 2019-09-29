c3a4fde7952fe0547f591cb14a048f4166a6ffc9
gangestad <36534289+gangestad@users.noreply.github.com>
2019-09-29 19:26:46 +0200

    Compulsory 2 start

---
+++ visualobject.cpp
@@ -0,0 +1,20 @@
+#include "visualobject.h"
+#include "shader.h"
+
+VisualObject::VisualObject()
+{
+}
+
+VisualObject::~VisualObject()
+{
+   glDeleteVertexArrays( 1, &mVAO );
+   glDeleteBuffers( 1, &mVBO );
+}
+
+void VisualObject::init()
+{
+}
+
+bool VisualObject::getUseTextures(){
+
+}
